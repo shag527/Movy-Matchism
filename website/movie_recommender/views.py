70872740a0da3f2ba4fed7b43e543d855b94246d
shag527 <shagunkatoch527@gmail.com>
2020-05-10 21:50:59 +0530

    home page created

--- website/movie_recommender/views.py
+++ website/movie_recommender/views.py
@@ -1,3 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponse
 
 # Create your views here.
+def home(request):
+	dict={'name':'Maze runner','rating':5}
+	return render(request,'home.html',dict)
--- website/website/urls.py
+++ website/website/urls.py
@@ -15,7 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.conf.urls import url
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    url(r'', include('movie_recommender.urls')),
 ]

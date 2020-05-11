82b86fd355bed1d8d4771378abcc2001377a2602
shag527 <shagunkatoch527@gmail.com>
2020-05-11 20:32:58 +0530

    Connected mysql

--- website/movie_recommender/urls.py
+++ website/movie_recommender/urls.py
@@ -1,7 +1,9 @@
 from django.conf.urls import url
 from . import views
+from django.contrib.auth.views import LoginView
 
 urlpatterns=[
     
-    url(r'',views.home),
+    url(r'home/',views.home),
+    url(r'login/',LoginView.as_view(template_name='login.html'),name="login"),
 ]
--- website/website/settings.py
+++ website/website/settings.py
@@ -74,13 +74,24 @@ WSGI_APPLICATION = 'website.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
+#DATABASES = {
+ #   'default': {
+ #       'ENGINE': 'django.db.backends.sqlite3',
+  #      'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+   # }
+#}
+
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'user_information',
+        'USER': 'root',
+        'PASSWORD': '',
+        'HOST': '127.0.0.1',
+        'PORT': '3306',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/3.0/ref/settings/#auth-password-validators
@@ -119,3 +130,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+LOGIN_REDIRECT_URL='/home/'

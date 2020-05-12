b1657cd1aa1bf2fc330792ae0bbaba6809d681fd
shag527 <shagunkatoch527@gmail.com>
2020-05-12 21:02:53 +0530

    Necessary changes made

--- website/movie_recommender/urls.py
+++ website/movie_recommender/urls.py
@@ -1,9 +1,13 @@
 from django.conf.urls import url
 from . import views
-from django.contrib.auth.views import LoginView
+from django.contrib.auth.views import LoginView, LogoutView
+
 
 urlpatterns=[
     
-    url(r'home/',views.home),
+    url(r'home/',views.home,name='home'),
+    url(r'profile/',views.profile,name='profile'),
     url(r'login/',LoginView.as_view(template_name='login.html'),name="login"),
+    url(r'logout/',LogoutView.as_view(next_page='home'),name="logout"),
+    url(r'^register/$',views.register,name='register'),
 ]

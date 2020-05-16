64f9b4a437734f87d1533451acfa8a658fc2d5fa
shag527 <shagunkatoch527@gmail.com>
2020-05-16 19:25:55 +0530

    API created

--- website/movie_recommender/models.py
+++ website/movie_recommender/models.py
@@ -8,3 +8,29 @@ class Contact_Us(models.Model):
 	email=models.EmailField(max_length=70,blank=True)
 	subject=models.CharField(default='',max_length=50)
 	message=models.TextField(max_length=500)
+
+
+class Movies_list(models.Model):
+	Rating_choices=(
+		(1,1),
+		(2,2),
+		(3,3),
+		(4,4),
+		(5,5),
+		)
+	movie1=models.CharField(max_length=40)
+	rating1=models.IntegerField(choices=Rating_choices,null=True,blank=True)
+	movie2=models.CharField(max_length=40,null=True,blank=True)
+	rating2=models.IntegerField(choices=Rating_choices,null=True,blank=True)
+	movie3=models.CharField(max_length=40,null=True,blank=True)
+	rating3=models.IntegerField(choices=Rating_choices,null=True,blank=True)
+	movie4=models.CharField(max_length=40,null=True,blank=True)
+	rating4=models.IntegerField(choices=Rating_choices,null=True,blank=True)
+	movie5=models.CharField(max_length=40,null=True,blank=True)
+	rating5=models.IntegerField(choices=Rating_choices,null=True,blank=True)
+
+	def __str__(self):
+		return self.movie1
+
+
+	
--- website/movie_recommender/urls.py
+++ website/movie_recommender/urls.py
@@ -1,6 +1,8 @@
 from django.conf.urls import url
+from django.urls import path, include
 from . import views
 from django.contrib.auth.views import LoginView, LogoutView
+from .views import recommend_movies
 
 
 urlpatterns=[
@@ -10,5 +12,7 @@ urlpatterns=[
     url(r'login/',LoginView.as_view(template_name='login.html'),name="login"),
     url(r'logout/',LogoutView.as_view(next_page='home'),name="logout"),
     url(r'^register/$',views.register,name='register'),
-    url(r'^recommendations/$',views.show_movies,name='show_movies')
+    url(r'^recommendations/$',views.show_movies,name='show_movies'),
+    path('recommend_movies/',recommend_movies.as_view()),
+    url(r'^recommend_movies/',views.recommend_movies,name='movies'),
 ]
